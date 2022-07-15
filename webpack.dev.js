fbdac04caa720bfd91fd6cce1c8e07127d117b98
Azeem <livelyazeem@gmail.com>
2022-07-15 11:47:19 +0500

    first commit

---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new ESLintPlugin(),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'project-ready-react-boilerplate',
+            inject: true,
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        hot: true,
+    },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: path.resolve(__dirname, './src/index.js'),
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new ESLintPlugin(),
+        new CleanWebpackPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'project-ready-react-boilerplate',
+            inject: true,
+            template: path.resolve(__dirname, './src/index.html'),
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        hot: true,
+    },
+};
