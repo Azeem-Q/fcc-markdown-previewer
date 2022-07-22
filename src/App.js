43528d53893ef682ef045f7b1bc4db2bef863b0b
Azeem <livelyazeem@gmail.com>
2022-07-22 12:12:50 +0500

    added editor and previewer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import './style.scss';
+import Markdown from 'marked-react';
+
+const initialText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+const Editor = (props) => {
+    return (
+        <textarea id="editor" value={props.text} onChange={props.onChange} />
+    );
+};
+
+const Previewer = (props) => {
+    return (
+        <div id="preview">
+            <Markdown>{props.result}</Markdown>
+        </div>
+    );
+};
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: '',
+            input: initialText,
         };
         this.handleChange = this.handleChange.bind(this);
     }
@@ -19,12 +79,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <textarea
-                    id="editor"
-                    value={this.state.input}
-                    onChange={this.handleChange}
-                />
-                <div id="previewer"></div>
+                <Editor text={this.state.input} onChange={this.handleChange} />
+                <Previewer result={this.state.input} />
             </div>
         );
     }
